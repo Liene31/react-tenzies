f12b6ac773c8940601f3feb1d094540d04cb94c9
Liene <lienebare@gmail.com>
2026-01-28 15:11:01 +0100

    add styling condition when toggles

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,8 +1,15 @@
 export const Die = (props) => {
-  console.log(props);
+  const diceClicked = {
+    backgroundColor: props.isHeld ? "green" : "#ffffff",
+  };
+
   return (
     <>
-      <button className="dice-btn" onClick={props.toggleDice}>
+      <button
+        className="dice-btn"
+        style={diceClicked}
+        onClick={props.toggleDice}
+      >
         {props.value}
       </button>
     </>
