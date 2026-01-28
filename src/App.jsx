647568a96cd212cb622df5d07ff1e70a12bfb7d3
Liene <lienebare@gmail.com>
2026-01-28 09:25:13 +0100

    add random dice array

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
 import { useState } from "react";
+import { nanoid } from "nanoid";
 import { Die } from "./Die.jsx";
 
 function App() {
-  return (
-    <main>
-      <Die value={1} />
-      <Die value={2} />
-      <Die value={3} />
-      <Die value={5} />
-      <Die value={5} />
-      <Die value={1} />
-      <Die value={1} />
-      <Die value={1} />
-      <Die value={1} />
-      <Die value={1} />
-    </main>
-  );
+  const [dice, setDice] = useState([1, 2, 3, 4, 5, 6, 1, 2, 3, 4]);
+
+  const generateRandomNo = () => {
+    return Math.ceil(Math.random() * 6);
+  };
+
+  const getAllRandomDice = () => {
+    const randomDice = [];
+    for (let i = 0; i < 10; i++) {
+      randomDice.push(generateRandomNo());
+    }
+
+    return randomDice;
+  };
+
+  console.log(getAllRandomDice());
+
+  const diceElement = dice.map((item) => {
+    return <Die key={nanoid()} value={item} />;
+  });
+
+  return <main>{diceElement}</main>;
 }
 
 export default App;
